a80389eaba197f0fc2811887cbb53895da1bc085
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-04-26 14:48:08 +0200

    updates

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,27 @@
-const token = localStorage.getItem("token");
+//så du ikke kan gå innpå admin uten å ha logget inn
 
-if(!token) {
-    window.location.href = "login.html";
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPage = window.location.pathname;
+    const token = localStorage.getItem("token");
+
+    if (currentPage.includes("admin.html") && !token) {
+        window.location.href = "../account/login.html"; // sender brukeren tilbake til login 
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // ser etter token i local storage
+    const token = localStorage.getItem("token");
+
+    // hvis den finner token blir du sendt til admin 
+    if (token) {
+        const signInLink = document.getElementById("signInLink");
+        signInLink.addEventListener("click", function(event) {
+            event.preventDefault(); // forhinderer å sende deg til login 
+            window.location.href = "../account/admin.html"; // Sender deg til admin 
+        });
+    }
+});
+
+
--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,19 @@
-window.onload = function () {
+//Hamburger mny
+
+function hamburgerMenu() {
     const menuBtns = document.querySelectorAll('.hamburger');
     const mobileMenu = document.querySelector('.nav-menu');
 
     menuBtns.forEach(function (menuBtn) {
         menuBtn.addEventListener('click', function () {
             menuBtn.classList.toggle('is-active');
-            mobileMenu.classList.toggle('is-active')
+            mobileMenu.classList.toggle('is-active');
             document.body.style.overflow = mobileMenu.classList.contains('is-active') ? 'hidden' : '';
         });
     });
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    hamburgerMenu();
+});
