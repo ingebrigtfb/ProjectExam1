7d668915667e06ebb582c89a857c4916020d5e3e
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-05-12 16:58:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ function displayCarouselPosts(posts) {
     slide.innerHTML = `
         </div>
         <div class="slide-content-inner">
-            <img src="${post.media.url} alt="${post.media.alt}">
+            <img src="${post.media.url}" alt="${post.media.alt}">
             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
             <a class="next" onclick="plusSlides(1)">&#10095;</a> 
             <div class="background-container"> 
